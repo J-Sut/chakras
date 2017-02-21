639ff2114a7a073dc4d40c4e18c2991f2bfeeeb7
j-Sut <flowstategame@gmail.com>
2017-02-21 12:01:48 -0800

    revision one of Chakras

---
+++ main.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(a,b) {
+	var result = a + b; 
+	return result;
+}
+
+var x = addTwoNumbers(5,10);
+addTwoNumbers(23,-10);
+addTwoNumbers(5,310);
+
+/* var name = prompt("what is your name?")
+console.log(name);
